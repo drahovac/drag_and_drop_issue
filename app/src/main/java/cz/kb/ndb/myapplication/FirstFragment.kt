b3a7ac3cb638096459a2deb1f07f406265c35993
Václav Drahorád <vaclav.drahorad@cleverlance.com>
2021-06-21 15:54:21 +0200

    Add basic drag and drop implementation

---
+++ app/src/main/java/cz/kb/ndb/myapplication/FirstFragment.kt
@@ -0,0 +1,68 @@
+package cz.kb.ndb.myapplication
+
+import android.content.ClipData
+import android.content.ClipDescription
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import cz.kb.ndb.myapplication.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        binding.buttonFirst.setOnLongClickListener { v ->
+            val item = ClipData.Item(v.tag as? CharSequence)
+
+            // Create a new ClipData using the tag as a label, the plain text MIME type, and
+            // the already-created item. This will create a new ClipDescription object within the
+            // ClipData, and set its MIME type entry to "text/plain"
+            val dragData = ClipData(
+                v.tag as? CharSequence,
+                arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
+                item
+            )
+
+            // Instantiates the drag shadow builder.
+            val myShadow = View.DragShadowBuilder(v)
+
+            // Starts the drag
+            v.startDrag(
+                dragData,   // the data to be dragged
+                myShadow,   // the drag shadow builder
+                null,       // no need to use local data
+                0           // flags (not currently used, set to 0)
+            )
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
